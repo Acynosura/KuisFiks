39c99b60464503ec59eb6b058df63d3f7ee90634
Acynosura <110158801+Acynosura@users.noreply.github.com>
2023-03-27 11:38:07 +0700

    Update Main2.java

--- Soal 1/Main2.java
+++ Soal 1/Main2.java
@@ -2,10 +2,9 @@ import java.util.Scanner;
 public class Main2 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        Circle C1 = new Circle();
-        C1.setColor("Yellow");
-        System.out.println(C1);
-        System.out.println("Pembatas \n");
+        Circle circ = new Circle();
+        circ.setColor("Green");
+        System.out.println(circ);
         Cylinder Tabung1 = new Cylinder(7);
         Cylinder Tabung2 = new Cylinder(7, 10 );
         Cylinder Tabung3 = new Cylinder(7, "Red", 10);
@@ -15,10 +14,10 @@ public class Main2 {
             System.out.println("Tabung ke " + (i+1));
             System.out.print("Masukkan jari jari baru : ");
             listTabung[i].setRadius(sc.nextDouble());
-            System.out.println("Tinggi tabung = " + listTabung[i].getRadius());
+            System.out.println("Radius tabung = " + listTabung[i].getRadius());
             System.out.print("Masukkan warna baru : ");
             listTabung[i].setColor(sc.next());
-            System.out.println("Tinggi tabung = " + listTabung[i].getColor());
+            System.out.println("Warna tabung = " + listTabung[i].getColor());
             System.out.print("Masukkan tinggi baru : ");
             listTabung[i].setHeight(sc.nextDouble());
             System.out.println("Tinggi tabung = " + listTabung[i].getHeight());
